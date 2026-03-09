26e699dd3cd70fdf0a730945ab78faef8f4b5fce
achesanticar <hsanticarreto@gmail.com>
2026-03-08 21:37:31 -0800

    Final version

--- PilaPeliculas.c
+++ PilaPeliculas.c
@@ -4,8 +4,8 @@
 
 typedef struct Peliculas
 {
-    int Cima;
-    char Titulo[10][100];
+    int Cima, Anno[10];
+    char Titulo[10][100], Director[10][50];
 }pelicula;
 
 pelicula *crear_pila(pelicula *P) {
@@ -18,16 +18,22 @@ void push(pelicula *P) {
     if (P->Cima == 9){printf("La pila esta llena");} 
     else {
         P->Cima++;
-        printf("Introduce el nombre de la pelicula: ");
+        printf("Nombre de la pelicula: ");
         getchar();
         fgets(P->Titulo[P->Cima], 100, stdin);
+        P->Titulo[P->Cima][strcspn(P->Titulo[P->Cima], "\n\r")] = 0;
+        printf("Director: ");
+        fgets(P->Director[P->Cima], 50, stdin);
+        P->Director[P->Cima][strcspn(P->Director[P->Cima], "\n\r")] = 0;
+        printf("\nAnno de lanzamiento: ");
+        scanf("%d", &P->Anno[P->Cima]);
     } 
 }
 
 void pop(pelicula *P) {
     if (P->Cima == -1) {printf("La pila esta vacia\n");} 
     else {      
-        printf("%s", P->Titulo[P->Cima]);
+        printf("%s, %s %d\n", P->Titulo[P->Cima], P->Director[P->Cima], P->Anno[P->Cima]);
         P->Cima--;
     }
 }
@@ -38,9 +44,9 @@ int main(int argc, char const *argv[])
     pelicula *P1;
     P1 = crear_pila(P1);
 
-    while (c != 2)
+    while (c != 3)
     {
-        printf("Elige una accion:\nPush-0\nPop-1\nSalir-2\n");
+        printf("Elige una accion:\nPush-0\nPop-1\nVer la pila completa-2\nSalir-3\n");
         scanf("%d", &c);
         switch (c)
         {
@@ -51,12 +57,19 @@ int main(int argc, char const *argv[])
         case 1:
             pop(P1);
             break;
-
+        
+        case 2:
+            for (int i = P1->Cima; i >= 0; i--)
+            {
+                printf("%d-%s, %s %d\n", i+1, P1->Titulo[i], P1->Director[i], P1->Anno[i]);
+            }
+            break;
         default:
             break;
         }
     }
     
+    free(P1);
     return 0;
 }
 
