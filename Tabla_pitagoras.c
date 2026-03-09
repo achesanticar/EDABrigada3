57477f47fea9849e99600a56fee85070562a3f30
achesanticar <hsanticarreto@gmail.com>
2026-03-09 13:52:22 -0600

    casifin

--- Tabla_pitagoras.c
+++ Tabla_pitagoras.c
@@ -11,15 +11,18 @@ int main(int argc, char const *argv[])
     scanf("%d", &Col);
 
     tab = (int **)malloc(Ren * sizeof(int*));
+
     for (int i = 0; i < Ren; i++)
     {
-            *tab= (int *)malloc(Col * sizeof(int));
+            tab[i]= (int *)malloc(Col * sizeof(int));
     }
     
     for (int i = 0; i < Ren ; i++)
     {
         for (int j = 0; j < Col; j++)
         {
+            i = (i == 0) ? 1 : i;
+            j = (j == 0) ? 1 : j;
             tab[i][j] = j * i;
         }
     }
