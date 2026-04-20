f066bbf1dcaa8dee2d490355f300ddcd03b47d26
achesanticar <hsanticarreto@gmail.com>
2026-04-19 18:33:41 -0600

    idk

--- Pila_BiDi.c
+++ Pila_BiDi.c
@@ -8,6 +8,7 @@ typedef struct Pila_Bidimensional
 }Pila;
 
 Pila *Inicializar_Pila (Pila *P) {
+    P = (Pila *)malloc(sizeof(Pila));
     P->s_top = -1;
     for (int i = 0; i < 10; i++)
     {
@@ -26,39 +27,39 @@ void Push_Pila (Pila *P) {
     }
 }
 
-void Push (Pila *P) {
-    if (P->top[P->s_top] == 9)
+void Push (Pila *P, float info) {
+    if (P->s_top == -1)
     {
-        printf("Pila llena; Operacion no disponible\n");
-    } else {
+        printf("No hay pilas creadas\n");
+    } else if (P->top[P->s_top] == 9)
+    {printf("Pila llena; Operacion no disponible\n");}
+     else {
         P->top[P->s_top]++;
-        printf("->");
-        scanf("%f", P->data[P->s_top][P->top[P->s_top]]);
+        P->data[P->s_top][P->top[P->s_top]] = info;
     } 
 }
 
 void Pop_Pila (Pila *P) {
     if (P->s_top == -1)
     {
-        printf("Pila de pilas vacia; Operacion no disponible");
-    } else if (P->top[P->s_top] == -1)
-    {
-        printf("Pila vacia; Operacion no disponible");
+        printf("Pila de pilas vacia; Operacion no disponible\n");
     } else {
         for (int i = 0; i <= P->top[P->s_top]; i++)
         {
-            printf("%d-> %.2f", i+1, P->data[P->s_top][P->top[P->s_top]]);
+            printf("%d-> %.2f\n", i+1, P->data[P->s_top][i]);
         }
         P->s_top--;
     }
 }
 
 void Pop (Pila *P) {
-    if (P->top[P->s_top] == -1)
+    if (P->s_top == -1)
     {
-        printf("Pila vacia; Operacion no disponible");
-    } else {
-        printf("%.2f", P->data[P->s_top][P->top[P->s_top]]);
+        printf("No hay pilas creadas\n");
+    } else if (P->top[P->s_top] == -1)
+    {printf("Pila vacia; Operacion no disponible\n");}
+    else {
+        printf("%.2f\n", P->data[P->s_top][P->top[P->s_top]]);
         P->top[P->s_top]--;
     }
 }
@@ -66,12 +67,15 @@ void Pop (Pila *P) {
 int main(int argc, char const *argv[])
 {
     Pila *P;
+    P = Inicializar_Pila(P);
     int opcion = 1;
+    float info;
 
     printf("--Pila bidimensional--\n");
     while (opcion != 0)
     {
-        printf("1-Push pila\n2-Push\n3-Pop pila\n4-Pop\n0-Salir");
+        printf("\n1-Push pila\n2-Push\n3-Pop pila\n4-Pop\n0-Salir\n");
+        scanf("%d", &opcion);
         switch (opcion)
         {
         case 1:
@@ -79,7 +83,9 @@ int main(int argc, char const *argv[])
             break;
 
         case 2:
-            Push(P);
+            printf("->");
+            scanf("%f", &info);
+            Push(P, info);
             break;
 
         case 3:
@@ -93,11 +99,13 @@ int main(int argc, char const *argv[])
         default:
             if (opcion != 0)
             {
-                printf("Eleccion invalida");
+                printf("\nEleccion invalida");
             }
             break;
         }
     }
     
+    free(P);
+
     return 0;
 }
